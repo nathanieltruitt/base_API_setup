728ed75b7c268251d8044c9be358d06119ea2617
Nathaniel Truitt <nathantruitt2@gmail.com>
2023-03-18 06:12:17 -0500

    setting up sidekiq and blueprinter initializers

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module BaseApiSetup
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
